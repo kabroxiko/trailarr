b9a69ce41372af3770949689089922951b236458
Hugo Araya Nash <kabroxiko@gmail.com>
2025-10-01 14:10:32 -0300

    Modularize internal package: move handlers, settings, sync, and types to separate files. Remove redeclarations and clean up imports.

---
+++ internal/types.go
@@ -0,0 +1,20 @@
+package internal
+
+const (
+	ConfigPath               = "/var/lib/extrazarr/config/config.yml"
+	MoviesCachePath          = "/var/lib/extrazarr/movies_cache.json"
+	MediaCoverPath           = "/var/lib/extrazarr/MediaCover/"
+	SeriesCachePath          = "/var/lib/extrazarr/series_cache.json"
+	ErrInvalidSonarrSettings = "Invalid Sonarr settings"
+	RemoteMediaCoverPath     = "/MediaCover/"
+	HeaderApiKey             = "X-Api-Key"
+	ErrInvalidRequest        = "invalid request"
+	HeaderContentType        = "Content-Type"
+)
+
+type SonarrSeries struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Year  int    `json:"year"`
+	Path  string `json:"path"`
+}
